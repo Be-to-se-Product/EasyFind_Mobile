624b9e119a50038415bc5a7bfc50e79c44e28210
PedroRCSilva <pedro.rsilva@sptech.school>
2024-03-15 23:09:08 -0300

    styleguide button and fonts apply global

--- app/src/main/java/com/easy/myapplication/Components.kt
+++ app/src/main/java/com/easy/myapplication/Components.kt
@@ -1,8 +1,23 @@
 package com.easy.myapplication
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
 
 @Composable
-fun Button(){
-
+fun ButtonCustomize(onClick : ()->Unit,  content: @Composable RowScope.() -> Unit
+){
+    Button(
+        onClick=onClick,content=content,
+        shape = RoundedCornerShape(20), modifier = Modifier.fillMaxWidth(fraction = 1f))
 }
--- app/src/main/java/com/easy/myapplication/Pages.kt
+++ app/src/main/java/com/easy/myapplication/Pages.kt
@@ -1,9 +1,22 @@
 package com.easy.myapplication
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.easy.myapplication.ui.theme.Poppins
 
 
 @Composable
 fun Login(){
+    
+    Column {
 
+        ButtonCustomize(onClick = { /*TODO*/ }) {
+            Text("Login", fontFamily = Poppins, fontWeight = FontWeight.SemiBold, fontSize = 16.sp)
+        }
+
+    }
+    
 }
