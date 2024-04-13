dbf941a9f096ca56fc69ec5d59c6abae2de4c613
PedroRCSilva <pedro.rsilva@sptech.school>
2024-04-13 00:13:20 -0300

    add display map with Bar and other components

--- app/src/main/java/com/easy/myapplication/Navigation.kt
+++ app/src/main/java/com/easy/myapplication/Navigation.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.easy.myapplication.screens.Login.Login
+import com.easy.myapplication.screens.Mapa.Mapa
 import com.easy.myapplication.shared.Header.Header
 
 
@@ -23,6 +24,9 @@ fun AppNavHost(
         composable("Login")
         {
             Login()
+        }
+                composable("Mapa"){
+            Mapa()
         }
     }
 }
