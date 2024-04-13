2d31265a4c813520d52ae7276f321ff6a67b1a5c
PedroRCSilva <pedro.rsilva@sptech.school>
2024-04-13 01:30:14 -0300

    finalizando codificação dos elementos do mobile

--- app/src/main/java/com/easy/myapplication/screens/Mapa/View.kt
+++ app/src/main/java/com/easy/myapplication/screens/Mapa/View.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -23,6 +24,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.easy.myapplication.R
 import com.easy.myapplication.shared.BarButton.BarButton
@@ -55,12 +57,254 @@ fun Mapa() {
                 )
             }
         }
+        BarButton(setShowBar = setShowBar, showBar = true) {
+            BarProducts()
+
+//            BarDirections()
+        }
+
     }
 }
 
 
 @Composable
-fun BarButtonsProducts(){
+fun Left(){
+    Column() {
+        Image(
+            painter = painterResource(id = R.mipmap.left),
+            contentDescription = "Marcador Inicio",
+            modifier = Modifier.size(30.dp)
+        )
+    }
+
+}
 
 
+@Composable
+fun Right(){
+    Column {
+        Image(
+            painter = painterResource(id = R.mipmap.right),
+            contentDescription = "Marcador Inicio",
+            modifier = Modifier.size(30.dp)
+        )
+    }
+
+}
+
+
+@Composable
+fun Siga(){
+    Column {
+        Image(
+            painter = painterResource(id = R.mipmap.up),
+            contentDescription = "Marcador Inicio",
+            modifier = Modifier.size(40.dp)
+        )
+    }
+
+}
+
+@Composable
+fun Back(){
+    Column {
+        Image(
+            painter = painterResource(id = R.mipmap.bottom),
+            contentDescription = "Marcador Inicio",
+            modifier = Modifier.size(40.dp)
+        )
+    }
+
+}
+@Composable
+@Preview
+fun BarDirections() {
+    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row {
+                    Title(content = "Carrefour Express Paulista")
+                }
+                Row {
+                    Subtitle(content = "4min(300m)", color = Primary)
+                }
+            }
+
+        Row {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(0.9f),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(30.dp)
+                ) {
+                    Left()
+                    Column(verticalArrangement = Arrangement.Center) {
+                        Subtitle(
+                            content = "Inicie seu trajéto e vire a direta a 200 métros",
+                            color = Primary
+                        )
+                    }
+                }
+            }
+        }
+        Row {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(0.9f),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(30.dp)
+                ) {
+                    Column {
+                        Right()
+                    }
+
+                    Column {
+                        Subtitle(
+                            content = "Inicie seu trajéto e vire a direta a 200 métros",
+                            color = Primary
+                        )
+                    }
+                }
+            }
+        }
+        Row {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(0.9f),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(30.dp)
+                ) {
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.mipmap.marked),
+                            contentDescription = "Marcador Inicio",
+                            modifier = Modifier.size(32.dp)
+                        )
+                    }
+
+                    Column(verticalArrangement = Arrangement.Center) {
+                        Subtitle(
+                            content = "Inicie seu trajéto e vire a direta a 200 métros",
+                            color = Primary
+                        )
+                    }
+                }
+            }
+        }
+
+
+    }
+
+}
+
+@Composable
+fun BarProducts() {
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(15.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp, 15.dp, 15.dp, 15.dp))
+                    .background(Color.Red)
+                    .width(70.dp)
+                    .height(80.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.mipmap.fone),
+                    contentDescription = "Imagem do produto",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Title(content = "Fone de ouvido")
+                    StarRatingBar(rating = 2F, size = 4F)
+                }
+                Row {
+                    Subtitle(content = "dfdfdf", color = Primary)
+                }
+                Row {
+                    Subtitle(content = "R$ 1220,00", color = Primary)
+                }
+
+
+                Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                    Column {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(10.dp)
+                        ) {
+                            Column {
+                                Image(
+                                    painter = painterResource(id = R.mipmap.cart),
+                                    contentDescription = "Icone",
+                                    modifier = Modifier.width(10.dp)
+                                )
+                            }
+                            Column {
+                                Subtitle(content = "15 min", color = Primary)
+                            }
+                        }
+                    }
+                    Column {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(10.dp)
+                        ) {
+                            Column {
+                                Image(
+                                    painter = painterResource(id = R.mipmap.cart),
+                                    contentDescription = "Icone",
+                                    modifier = Modifier.width(10.dp)
+                                )
+                            }
+                            Column {
+                                Subtitle(content = "15 min", color = Primary)
+                            }
+                        }
+                    }
+                    Column {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(10.dp)
+                        ) {
+                            Column {
+                                Image(
+                                    painter = painterResource(id = R.mipmap.cart),
+                                    contentDescription = "Icone",
+                                    modifier = Modifier.width(10.dp)
+                                )
+                            }
+                            Column {
+                                Subtitle(content = "15 min", color = Primary)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
