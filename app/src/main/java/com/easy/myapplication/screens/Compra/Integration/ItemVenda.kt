d53c7d362dde6ecac195bb60fcec6bae548915a3
pedrohmunizs <111442128+pedrohmunizs@users.noreply.github.com>
2024-06-12 02:16:23 -0300

    Update ItemVenda.kt

--- app/src/main/java/com/easy/myapplication/screens/Compra/Integration/ItemVenda.kt
+++ app/src/main/java/com/easy/myapplication/screens/Compra/Integration/ItemVenda.kt
@@ -3,8 +3,4 @@ package com.easy.myapplication.screens.Compra.Integration
 class ItemVenda {
     var idProduto:Long? = null
     var quantidade:Int = 1
-
-    override fun toString(): String {
-        return "ItemVenda(idProduto='$idProduto', quantidade=$quantidade)"
-    }
 }
